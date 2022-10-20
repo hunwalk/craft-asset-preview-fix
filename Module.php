9f8ab2dd279ba42b2ab50105ae2c1bab168ff68e
Horváth Gergely <horg3r@gmail.com>
2022-10-20 18:32:05 +0200

    feat: asset preview module

--- Module.php
+++ Module.php
@@ -15,28 +15,27 @@ class Module extends \yii\base\Module
 {
     public function init()
     {
-        parent::init();
-
         $this->controllerNamespace = 'modules\\assetpreview\\controllers';
+
+        Event::on(UrlManager::class,UrlManager::EVENT_REGISTER_CP_URL_RULES, function (RegisterUrlRulesEvent $event){
+            $event->rules['asset-preview/default/index'] = 'asset-preview/default/index';
+        });
 
         Event::on(Asset::class, Asset::EVENT_DEFINE_URL, function (DefineAssetUrlEvent $event){
             $volume = $event->asset->getVolume();
             if (!$volume->getFs()->hasUrls){
-                $event->url = '/admin/asset-preview/default/index?assetId=' . $event->asset->id;
+                $event->url = '/useful/access/asset?uid='.$event->asset->uid.'&sendFile=0';
             }
         });
 
         Event::on(Assets::class, Assets::EVENT_DEFINE_THUMB_URL, function (DefineAssetThumbUrlEvent $event){
             $volume = $event->asset->getVolume();
             if (!$volume->getFs()->hasUrls){
-                $event->url = '/admin/asset-preview/default/index?assetId=' . $event->asset->id;
+                $event->url = '/useful/access/asset-thumb?uid='.$event->asset->uid.'&sendFile=0';
             }
         });
 
-        Event::on(UrlManager::class,UrlManager::EVENT_REGISTER_CP_URL_RULES, function (RegisterUrlRulesEvent $event){
-            $event->rules['asset-preview/default/index'] = 'asset-preview/default/index';
-        });
-
+        parent::init();
 
     }
 }
